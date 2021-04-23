96804a8915bea7453aa6675fca0e14bcaceedc8d
brewea <56275753+brewea@users.noreply.github.com>
2021-04-23 09:32:05 -0400

    cpp and txt uploaded / Finished

---
+++ Anagram Finder/anagram.cpp
@@ -0,0 +1,78 @@
+/*
+ * Name: Carter Brewer  
+ * Date Submitted: 10 March 2021
+ * Assignment Name: Single-Word Anagram Finder
+ */
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+vector<string> loadWordlist(string filename);
+
+/*Implement the following function:
+  anagram() takes a word (string) and a wordlist of all words (vector of strings)
+  and builds a dictionary/map where the key is a specific number of times each
+  letter occurs in a word and the associated value is a vector of strings
+  containing all words using those letters (anagrams).
+  The value returned is a vector of strings containing the corresponding
+  set of anagrams
+*/
+vector<string> anagram(string word, vector<string> wordlist);
+
+int main()
+{
+    vector<string> words;
+    vector<string> anagrams;
+    string inputWord;
+    words=loadWordlist("wordlist.txt");
+    cout << "Find single-word anagrams for the following word: ";
+    cin >> inputWord;
+    anagrams = anagram(inputWord, words);
+    for (int i=0; i<anagrams.size(); i++)
+    {
+        cout << anagrams[i] << endl;
+    }
+    return 0;
+}
+
+vector<string> loadWordlist(string filename)
+{
+    vector<string> words;
+    ifstream inFile;
+    string word;
+    inFile.open(filename);
+    if(inFile.is_open())
+    {
+        while(getline(inFile,word))
+        {
+            if(word.length() > 0)
+            {
+                words.push_back(word);
+            }
+        }
+        inFile.close();
+    }
+    return words;
+}
+
+//Implement this function
+vector<string> anagram(string word, vector<string> wordlist)
+{
+    string anagram = word;
+    vector<string> anagrams;
+
+    sort(word.begin(), word.end());
+    do
+    {
+        for (int i = 0; i < wordlist.size(); i++)
+        {
+            if (word == wordlist[i]){anagrams.push_back(word);}
+        }
+    } while (next_permutation(word.begin(), word.end()));
+    return anagrams;
+}
